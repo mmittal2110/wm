875d6aab991807b64512da517b36fd64b6042674
manish <manish.mittal@oneassist.in>
2019-08-25 12:12:23 +0530

    logo component

--- src/components/header-component.js
+++ src/components/header-component.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Logo from "./logo-component";
 class Header extends Component {
     render() {
         return (
             <div>
                 Header component
+                <Logo />
             </div>
         );
     }
